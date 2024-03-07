6fafa32e388474b11a5ab5aa20e9c1511b5bbc03
AdrianAasen <aadrian.aasen@gmail.com>
2024-03-07 18:07:36 +0100

    Added initial unittesting

---
+++ tests/test_POVM.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+from functools import reduce
+import sys
+sys.path.append('../') # Adding path to library
+from EMQST_lib.povm import POVM
+
+class testPOVM(unittest.TestCase):
+    
+    def test_getting(self):
+        simple_POVM = np.array([[[1,0],[0,0]],[[0,0],[0,1]]])
+        angles = np.array([[0,0],[np.pi/2,0]])
+        test = POVM(simple_POVM,angles)
+        self.assertTrue(np.all(simple_POVM == test.get_POVM()),"Setting and getting does not work.")
+        
+        self.assertTrue(np.all(test.get_angles()==angles))
+        povm = POVM.computational_basis_POVM(1)
+        self.assertTrue(np.all(povm[0].get_POVM()==simple_POVM))
+    
+    def test_angles(self):
+        povm = POVM.computational_basis_POVM(1)[0]
+        angles = np.array([[[0,0]],[[np.pi,0]]])
+        angles_povm = povm.get_angles()
+        self.assertTrue(np.all(angles == angles_povm))
+    
+    def test_pauli(self):
+        pauli = POVM.generate_Pauli_POVM(1)
+        comp_basis = POVM.computational_basis_POVM(1)[0]
+        y = np.array([[[0.5,-0.5j],[0.5j,0.5]],[[0.5,0.5j],[-0.5j,0.5]]], dtype = complex)
+        # Check z measurement is equal to comp. basis
+        self.assertTrue(np.all(pauli[2].get_POVM() == comp_basis.get_POVM()))
+        self.assertTrue(np.all(pauli[2].get_angles() == comp_basis.get_angles()))
+        self.assertTrue(np.all(pauli[1].get_POVM() == y))
+        
+        
+        
---
+++ tests/test_hashing.py
@@ -0,0 +1,79 @@
+import unittest
+import numpy as np
+from functools import reduce
+import sys
+sys.path.append('../') # Adding path to library
+from EMQST_lib import support_functions as sf
+
+
+
+
+
+class TestHash(unittest.TestCase):
+    
+    
+    def test_downconvert(self):
+        # Test that 2 qubit downconvertion works
+        test = np.arange(16)
+        qubit_index_1 = np.array([1,2])
+
+        self.assertTrue(np.all(sf.downconvert_data(qubit_index_1,test)==np.array([0,0,1,1,2,2,3,3,0,0,1,1,2,2,3,3])),"Downcovert does not mach ideal case.")
+        qubit_index_2 = np.array([0,3])
+        self.assertTrue(np.all(sf.downconvert_data(qubit_index_2,test)==np.array([0,1,0,1,0,1,0,1,2,3,2,3,2,3,2,3])),"Downcovert does not mach ideal case.")
+        
+    def test_downconvert_false(self):
+        # Test that 2 qubit downconvertion works
+        test = np.arange(16)
+        qubit_index_1 = np.array([0,2])
+        self.assertFalse(np.all(sf.downconvert_data(qubit_index_1,test)==np.array([0,0,1,1,2,2,3,3,0,0,1,1,2,2,3,3])), "Should be wrong")
+
+    def test_downcovert_inverse_order(self):
+        # Checks if inverse order matter
+        test = np.arange(16)
+        self.assertTrue(np.all(sf.downconvert_data(np.array([1,0]),test) == sf.downconvert_data(np.array([0,1]),test)),"Index order is not followed.")
+
+
+
+    def test_calibration_states_generation(self):
+        """
+        This test ensures that the ordering of the hashing function is correct. 
+        """
+        one_qubit_calibration_angles = np.array([[[0,0]],[[np.pi,0]]])
+        one_qubit_calibration_states = np.array([sf.get_density_matrix_from_angles(angle) for angle in one_qubit_calibration_angles])
+        hash = np.array([1,0])
+        test = sf.generate_calibration_states_from_hash(hash,one_qubit_calibration_states)
+        self.assertEqual(test[0,0,0],1)
+        self.assertEqual(test[1,1,1],1)
+        self.assertEqual(test[2,2,2],1)
+        self.assertEqual(test[3,3,3],1)
+        
+        hash = np.array([0,1])
+        test = sf.generate_calibration_states_from_hash(hash,one_qubit_calibration_states)
+        self.assertEqual(test[0,0,0],1)
+        self.assertEqual(test[1,2,2],1)
+        self.assertEqual(test[2,1,1],1)
+        self.assertEqual(test[3,3,3],1)
+        
+        hash = np.array([1,1])
+        test = sf.generate_calibration_states_from_hash(hash,one_qubit_calibration_states)
+        self.assertEqual(test[0,0,0],1)
+        self.assertEqual(test[1,0,0],1)
+        self.assertEqual(test[2,3,3],1)
+        self.assertEqual(test[3,3,3],1)
+        
+        # Test that multiple hashes work 
+        hash = np.array([[1,0,1],[1,0,1]])
+        test = np.array([sf.generate_calibration_states_from_hash(function,one_qubit_calibration_states) for function in hash])
+        self.assertEqual(test[0,0,0,0],1)
+        self.assertEqual(test[0,1,2,2],1)
+        self.assertEqual(test[0,2,5,5],1)
+        self.assertEqual(test[0,3,7,7],1)
+        self.assertEqual(test[1,0,0,0],1)
+        self.assertEqual(test[1,1,2,2],1)
+        
+        
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
