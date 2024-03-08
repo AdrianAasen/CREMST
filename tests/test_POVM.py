d78b008161e44156413673e1ab0ef7ebd79b0418
AdrianAasen <aadrian.aasen@gmail.com>
2024-03-08 10:56:13 +0100

    Added POVM test

--- tests/test_POVM.py
+++ tests/test_POVM.py
@@ -27,10 +27,14 @@ class testPOVM(unittest.TestCase):
         pauli = POVM.generate_Pauli_POVM(1)
         comp_basis = POVM.computational_basis_POVM(1)[0]
         y = np.array([[[0.5,-0.5j],[0.5j,0.5]],[[0.5,0.5j],[-0.5j,0.5]]], dtype = complex)
+        x = np.array([[[0.5,0.5j],[0.5,0.5]],[[0.5,-0.5],[-0.5,0.5]]], dtype = complex)
         # Check z measurement is equal to comp. basis
         self.assertTrue(np.all(pauli[2].get_POVM() == comp_basis.get_POVM()))
         self.assertTrue(np.all(pauli[2].get_angles() == comp_basis.get_angles()))
         self.assertTrue(np.all(pauli[1].get_POVM() == y))
+        self.assertTrue(np.all(pauli[1].get_POVM() == x))
+        
+        
         
         
         
